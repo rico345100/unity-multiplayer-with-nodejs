66023581180c698b84a94580c8a97ff3083c0a36
modernator <rico345100@gmail.com>
2019-03-13 23:42:11 +0900

    Added PlayerScript

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+	void Start() {
+		NetworkObject networkObject = GetComponent<NetworkObject>();
+		
+		if(!networkObject.isLocal) {
+			Destroy(GetComponent<Dot_Truck_Controller>());
+			Destroy(transform.Find("Main Camera").gameObject);
+		}
+	}
+}
